e94aece09e11f50c38d21b3c160c31524b06010c
Aira <dolfa226@newschool.edu>
2019-12-11 22:19:27 -0500

    transparent and rotation

--- assets/script.js
+++ assets/script.js
@@ -24,44 +24,87 @@ document.body.appendChild( renderer.domElement );
 
 
 //cylinder1 - mesh
-var geometry = new THREE.CylinderGeometry(3, 3, 3);
+var geometry1 = new THREE.CylinderGeometry(.5, .5, .5);
+var geometry2 = new THREE.CylinderGeometry(5, 5, 5);
+var geometry3 = new THREE.CylinderGeometry(10, 10, 10);
 
 var texture = new THREE.TextureLoader().load( 'assets/imgs/ticker.svg' );
 //texture
-var material = new THREE.MeshBasicMaterial( { map: texture } 
-	);
+var materials = [
+	
+	new THREE.MeshBasicMaterial( { 
+	map: texture,
+	transparent: true,
+	alphaTest: 0.5,
+	side: THREE.DoubleSide,	
+	}),
 
-var cylinder = new THREE.Mesh( geometry, material );
-scene.add( cylinder );
+	new THREE.MeshPhongMaterial( { 
+	map: texture,
+	transparent: true,
+	alphaTest: 0.5,
+	opacity: 0,
+	side: THREE.DoubleSide, 
+	}), 
+
+];
+
+
+var cylinder1 = new THREE.Mesh( geometry1, materials );
+scene.add( cylinder1 );
+var cylinder2 = new THREE.Mesh( geometry2, materials );
+scene.add( cylinder2 );
+var cylinder3 = new THREE.Mesh( geometry3, materials );
+scene.add( cylinder3 );
+
 
 if(localStorage.getItem("rotation")){
-	cylinder.rotation.y = parseFloat(localStorage.getItem("rotation"));
+	cylinder1.rotation.y = parseFloat(localStorage.getItem("rotation"));
 }
 
 
-/*
-integer 0, 1
+if(localStorage.getItem("rotation")){
+	cylinder2.rotation.y = parseFloat(localStorage.getItem("rotation"));
+}
 
-float 0.2345678
-
-*/
 
 
 function render() {
 	requestAnimationFrame(render);
 	
-
-	
-	
 	// cylinder.rotation.x += 0.01;
-	  cylinder.rotation.y += 0.001;
+	  cylinder1.rotation.y += 0.03;
+	  cylinder2.rotation.y += 0.01;
+	  cylinder3.rotation.y += 0.01;
 	//   console.log(camera.position.z)
 	//   console.log(cylinder.rotation.y)
 
 	localStorage.setItem('cameraX', "" + camera.position.x)
 	localStorage.setItem('cameraY', "" + camera.position.y)
 	localStorage.setItem('cameraZ', "" + camera.position.z)
-	localStorage.setItem('rotation', "" + cylinder.rotation.y)
+	localStorage.setItem('rotation', "" + cylinder1.rotation.y)
+
+
+	renderer.render(scene, camera);
+}
+render();
+
+function render() {
+	requestAnimationFrame(render);
+	
+	// cylinder.rotation.x += 0.01;
+	  cylinder1.rotation.y += 0.03;
+	  cylinder2.rotation.y += 0.01;
+	  cylinder3.rotation.y += 0.001;
+	//   console.log(camera.position.z)
+	//   console.log(cylinder.rotation.y)
+
+	localStorage.setItem('cameraX', "" + camera.position.x)
+	localStorage.setItem('cameraY', "" + camera.position.y)
+	localStorage.setItem('cameraZ', "" + camera.position.z)
+	localStorage.setItem('rotation', "" + cylinder1.rotation.y)
+	localStorage.setItem('rotation', "" + cylinder2.rotation.y)
+	localStorage.setItem('rotation', "" + cylinder3.rotation.y)
 
 	renderer.render(scene, camera);
 }
