3aeea7e10ccd280c320601c8d7d580eedd226776
Aira <dolfa226@newschool.edu>
2019-12-13 09:07:35 -0500

    121019

--- assets/script.js
+++ assets/script.js
@@ -28,15 +28,11 @@ var geometry1 = new THREE.CylinderGeometry(.5, .5, .5, 20);
 var geometry2 = new THREE.CylinderGeometry(3, 3, 3, 20);
 var geometry3 = new THREE.CylinderGeometry(10, 10, 10, 20);
 var geometry4 = new THREE.CylinderGeometry(20, 20, 20, 20);
-var geometry5 = new THREE.CylinderGeometry(30, 30, 30, 20);
-var geometry6 = new THREE.CylinderGeometry(50, 50, 50, 20);
 
 var texture1 = new THREE.TextureLoader().load( 'assets/imgs/seconds.svg' );
 var texture2 = new THREE.TextureLoader().load( 'assets/imgs/minutes.svg' );
 var texture3 = new THREE.TextureLoader().load( 'assets/imgs/hours.svg' );
 var texture4 = new THREE.TextureLoader().load( 'assets/imgs/days.svg' );
-var texture5 = new THREE.TextureLoader().load( 'assets/imgs/weeks.svg' );
-var texture6 = new THREE.TextureLoader().load( 'assets/imgs/months.svg' );
 
 var materials1 = [
 
@@ -115,44 +111,6 @@ var materials4 = [
 
 ];
 
-var materials5 = [
-
-	new THREE.MeshBasicMaterial( { 
-	map: texture5,
-	transparent: true,
-	alphaTest: 0.5,
-	side: THREE.DoubleSide,	
-	}),
-
-	new THREE.MeshPhongMaterial( { 
-	map: texture5,
-	transparent: true,
-	alphaTest: 0.5,
-	opacity: 0,
-	side: THREE.DoubleSide, 
-	}), 
-
-];
-
-var materials6 = [
-
-	new THREE.MeshBasicMaterial( { 
-	map: texture6,
-	transparent: true,
-	alphaTest: 0.5,
-	side: THREE.DoubleSide,	
-	}),
-
-	new THREE.MeshPhongMaterial( { 
-	map: texture6,
-	transparent: true,
-	alphaTest: 0.5,
-	opacity: 0,
-	side: THREE.DoubleSide, 
-	}), 
-
-];
-
 var cylinder1 = new THREE.Mesh( geometry1, materials1 );
 scene.add( cylinder1 );
 var cylinder2 = new THREE.Mesh( geometry2, materials2 );
@@ -161,10 +119,7 @@ var cylinder3 = new THREE.Mesh( geometry3, materials3 );
 scene.add( cylinder3 );
 var cylinder4 = new THREE.Mesh( geometry4, materials4 );
 scene.add( cylinder4 );
-var cylinder5 = new THREE.Mesh( geometry5, materials5 );
-scene.add( cylinder5 );
-var cylinder6 = new THREE.Mesh( geometry6, materials6 );
-scene.add( cylinder6 );
+
 
 
 if(localStorage.getItem("rotation")){
@@ -184,13 +139,7 @@ if(localStorage.getItem("rotation")){
 	cylinder4.rotation.y = parseFloat(localStorage.getItem("rotation"));
 }
 
-if(localStorage.getItem("rotation")){
-	cylinder5.rotation.y = parseFloat(localStorage.getItem("rotation"));
-}
 
-if(localStorage.getItem("rotation")){
-	cylinder6.rotation.y = parseFloat(localStorage.getItem("rotation"));
-}
 
 // function render() {
 // 	requestAnimationFrame(render);
@@ -216,19 +165,13 @@ if(localStorage.getItem("rotation")){
 function render() {
 	requestAnimationFrame(render);
 	
-	var seconds = 0.001;
-	var minutes = seconds/60;
-	var hours = minutes/60;
-	var days = hours/24;
-	var weeks = days/7;
-	var months = months/12;
-
-	  cylinder1.rotation.y += seconds;
-	  cylinder2.rotation.y += minutes;
-	  cylinder3.rotation.y += hours;
-	  cylinder4.rotation.y += days;
-	  cylinder5.rotation.y += weeks;
-	  cylinder6.rotation.y += months;
+	// cylinder.rotation.x += 0.01;
+	  cylinder1.rotation.y += 0.03;
+	  cylinder2.rotation.y += 0.01;
+	  cylinder3.rotation.y += 0.001;
+	  cylinder4.rotation.y += 0.0001;
+	//   console.log(camera.position.z)
+	//   console.log(cylinder.rotation.y)
 
 	localStorage.setItem('cameraX', "" + camera.position.x)
 	localStorage.setItem('cameraY', "" + camera.position.y)
@@ -237,14 +180,11 @@ function render() {
 	localStorage.setItem('rotation', "" + cylinder2.rotation.y)
 	localStorage.setItem('rotation', "" + cylinder3.rotation.y)
 	localStorage.setItem('rotation', "" + cylinder4.rotation.y)
-	localStorage.setItem('rotation', "" + cylinder5.rotation.y)
-	localStorage.setItem('rotation', "" + cylinder6.rotation.y)
 
 	renderer.render(scene, camera);
-		// localStorage.clear();
 }
 render();
 
 
-
+// localStorage.setItem("position", JSON.Stringify(thePosition));
 
