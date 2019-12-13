a994aeca0a2797969b0248147d1e2b81ad4f26f1
Aira <dolfa226@newschool.edu>
2019-12-13 08:55:08 -0500

    fixed

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,5 @@
 var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 01, 1000);
+var camera = new THREE.PerspectiveCamera(40, window.innerWidth / window.innerHeight, 01, 1000);
 camera.position.set(10,2.5,10);
 
 console.log(camera.position);
@@ -24,20 +24,15 @@ document.body.appendChild( renderer.domElement );
 
 
 //cylinder1 - mesh
-var geometry1 = new THREE.CylinderGeometry(5, 5, 5, 15);
-var geometry2 = new THREE.CylinderGeometry(10, 10, 10, 15);
-var geometry3 = new THREE.CylinderGeometry(20, 20, 20, 15);
-var geometry4 = new THREE.CylinderGeometry(30, 30, 30, 30);
-var geometry5 = new THREE.CylinderGeometry(40, 40, 40, 30);
-var geometry6 = new THREE.CylinderGeometry(50, 50, 50, 30);
+var geometry1 = new THREE.CylinderGeometry(.5, .5, .5, 20);
+var geometry2 = new THREE.CylinderGeometry(3, 3, 3, 20);
+var geometry3 = new THREE.CylinderGeometry(10, 10, 10, 20);
+var geometry4 = new THREE.CylinderGeometry(20, 20, 20, 20);
 
 var texture1 = new THREE.TextureLoader().load( 'assets/imgs/seconds.svg' );
-// texture4.anisotropy = renderer.getMaxAnisotropy();
 var texture2 = new THREE.TextureLoader().load( 'assets/imgs/minutes.svg' );
 var texture3 = new THREE.TextureLoader().load( 'assets/imgs/hours.svg' );
 var texture4 = new THREE.TextureLoader().load( 'assets/imgs/days.svg' );
-var texture5 = new THREE.TextureLoader().load( 'assets/imgs/weeks.svg' );
-var texture6 = new THREE.TextureLoader().load( 'assets/imgs/minutes.svg' );
 
 var materials1 = [
 
@@ -104,51 +99,10 @@ var materials4 = [
 	transparent: true,
 	alphaTest: 0.5,
 	side: THREE.DoubleSide,	
-	
 	}),
 
 	new THREE.MeshPhongMaterial( { 
 	map: texture4,
-	transparent: true,
-	alphaTest: 0.5,
-	opacity: 0,
-	side: THREE.DoubleSide, 
-	}), 
-
-];
-
-var materials5 = [
-
-	new THREE.MeshBasicMaterial( { 
-	map: texture5,
-	transparent: true,
-	alphaTest: 0.5,
-	side: THREE.DoubleSide,	
-	
-	}),
-
-	new THREE.MeshPhongMaterial( { 
-	map: texture5,
-	transparent: true,
-	alphaTest: 0.5,
-	opacity: 0,
-	side: THREE.DoubleSide, 
-	}), 
-
-];
-
-var materials6 = [
-
-	new THREE.MeshBasicMaterial( { 
-	map: texture6,
-	transparent: true,
-	alphaTest: 0.5,
-	side: THREE.DoubleSide,	
-	
-	}),
-
-	new THREE.MeshPhongMaterial( { 
-	map: texture6,
 	transparent: true,
 	alphaTest: 0.5,
 	opacity: 0,
@@ -165,10 +119,6 @@ var cylinder3 = new THREE.Mesh( geometry3, materials3 );
 scene.add( cylinder3 );
 var cylinder4 = new THREE.Mesh( geometry4, materials4 );
 scene.add( cylinder4 );
-var cylinder5 = new THREE.Mesh( geometry5, materials5 );
-scene.add( cylinder5 );
-var cylinder6 = new THREE.Mesh( geometry6, materials6 );
-scene.add( cylinder6 );
 
 
 
@@ -189,32 +139,39 @@ if(localStorage.getItem("rotation")){
 	cylinder4.rotation.y = parseFloat(localStorage.getItem("rotation"));
 }
 
-if(localStorage.getItem("rotation")){
-	cylinder5.rotation.y = parseFloat(localStorage.getItem("rotation"));
-}
-
-if(localStorage.getItem("rotation")){
-	cylinder6.rotation.y = parseFloat(localStorage.getItem("rotation"));
-}
 
 
+// function render() {
+// 	requestAnimationFrame(render);
+	
+// 	// cylinder.rotation.x += 0.01;
+// 	  cylinder1.rotation.y += 0.03;
+// 	  cylinder2.rotation.y += 0.01;
+// 	  cylinder3.rotation.y += 0.01;
+// 	  cylinder4.rotation.y += 0.01;
+// 	//   console.log(camera.position.z)
+// 	//   console.log(cylinder.rotation.y)
+
+// 	localStorage.setItem('cameraX', "" + camera.position.x)
+// 	localStorage.setItem('cameraY', "" + camera.position.y)
+// 	localStorage.setItem('cameraZ', "" + camera.position.z)
+// 	localStorage.setItem('rotation', "" + cylinder1.rotation.y)
+
+
+// 	renderer.render(scene, camera);
+// }
+// render();
 
 function render() {
 	requestAnimationFrame(render);
-	var seconds = 0.001;
-	var minutes = seconds/60;
-	var hours = minutes/60;
-	var days = hours/24;
-	var weeks = days/7;
-	var months = months/12;
-
-	  cylinder1.rotation.y += seconds;
-	  cylinder2.rotation.y += minutes;
-	  cylinder3.rotation.y += hours;
-	  cylinder4.rotation.y += days;
-	  cylinder5.rotation.y += weeks;
-	  cylinder6.rotation.y += months;
-
+	
+	// cylinder.rotation.x += 0.01;
+	  cylinder1.rotation.y += 0.03;
+	  cylinder2.rotation.y += 0.01;
+	  cylinder3.rotation.y += 0.001;
+	  cylinder4.rotation.y += 0.0001;
+	//   console.log(camera.position.z)
+	//   console.log(cylinder.rotation.y)
 
 	localStorage.setItem('cameraX', "" + camera.position.x)
 	localStorage.setItem('cameraY', "" + camera.position.y)
@@ -223,12 +180,11 @@ function render() {
 	localStorage.setItem('rotation', "" + cylinder2.rotation.y)
 	localStorage.setItem('rotation', "" + cylinder3.rotation.y)
 	localStorage.setItem('rotation', "" + cylinder4.rotation.y)
-	localStorage.setItem('rotation', "" + cylinder5.rotation.y)
-	localStorage.setItem('rotation', "" + cylinder6.rotation.y)
-	
+
 	renderer.render(scene, camera);
-	// localStorage.clear();
 }
 render();
 
+
+// localStorage.setItem("position", JSON.Stringify(thePosition));
 
